72c408ac7ac42aef680f9c088f5524ac0db75044
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-12-04 18:16:55 +0800

    fix

--- app/Jobs/UpdateCoin.php
+++ app/Jobs/UpdateCoin.php
@@ -102,4 +102,15 @@ class UpdateCoin implements ShouldQueue
             );
         }
     }
+
+    private function getPercentChange($oldNumber, $newNumber)
+    {
+        if ($oldNumber == 0) {
+            return (float) 0;
+        } else {
+            $changeValue = $newNumber - $oldNumber;
+
+            return (float) ($changeValue / $oldNumber) * 100;
+        }
+    }
 }
