72ad2f5cd7fe03790d765eb55a7c034764336ba4
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-12-07 10:39:46 +0800

    fix

--- database/migrations/2017_11_23_173021_create_prices_table.php
+++ database/migrations/2017_11_23_173021_create_prices_table.php
@@ -16,18 +16,18 @@ class CreatePricesTable extends Migration
         Schema::create('prices', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('coin_id')->unsigned();
-            $table->decimal('price_btc', 30, 20)->nullable();
-            $table->decimal('price_usd', 30, 20)->nullable();
-            $table->decimal('volume', 30, 20)->nullable();
+            $table->decimal('price_btc', 60, 20)->nullable();
+            $table->decimal('price_usd', 60, 20)->nullable();
+            $table->decimal('volume', 60, 20)->nullable();
             $table->bigInteger('supply')->nullable();
             $table->bigInteger('market_cap')->nullable();
-            $table->decimal('percent_market', 30, 20);
-            $table->decimal('percent_volume', 30, 20);
-            $table->decimal('percent_btc', 30, 20);
-            $table->decimal('percent_usd', 30, 20);
-            $table->decimal('btc_s', 30, 20);
-            $table->decimal('usd_s', 30, 20);
-            $table->decimal('speed', 30, 20);
+            $table->decimal('percent_market', 60, 20);
+            $table->decimal('percent_volume', 60, 20);
+            $table->decimal('percent_btc', 60, 20);
+            $table->decimal('percent_usd', 60, 20);
+            $table->decimal('btc_s', 60, 20);
+            $table->decimal('usd_s', 60, 20);
+            $table->decimal('speed', 60, 20);
             $table->timestamps();
         });
     }
